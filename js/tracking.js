4d644a0e5c4048f33e02ffffa2993d1b62081447
anibal <acopitan@gmail.com>
2013-06-04 00:37:38 -0500

    traking optimizado

--- js/tracking.js
+++ js/tracking.js
@@ -8,92 +8,92 @@ function cearCookie(nombre, valor, caducidad){
 }
 
 function leerCookie(nombre) {
+  
 	var buscamos= nombre + "=";//Tracking
-	if(document.cookie.length > 0)
-	{i= document.cookie.indexOf(buscamos);
 
+	if(document.cookie.length > 0){
+      i = document.cookie.indexOf(buscamos);
+      
   		if( i!=-1 ){
-			i+= buscamos.length;  //22
-			j= document.cookie.indexOf(";", i);
-			if( j==-1 )	j= document.cookie.length;
-			return unescape(document.cookie.substring(i, j));
+    		i+= buscamos.length;  //22
+    		j= document.cookie.indexOf(";", i);
+    		if( j==-1 )	j= document.cookie.length;
+    		return unescape(document.cookie.substring(i, j));
   		}
-	}
+	}else{
+    return false;
+  }
+
 }
 
 // Lee o crea tracking
 var cargarPag = function (){
-	var temp;
-	var pag_ant;
-        var dias = 30; //dÃ­as a los que caduca la cookie
+  var temp='';
+  var pag_ant;
+  var dias = 30; //dÃ­as a los que caduca la cookie
 
-	if(leerCookie("Tracking")=="" || leerCookie("Tracking")==undefined){
-            var cad = new Date(new Date().setTime(new Date().getTime()+dias*24*60*60*1000)); // 30 days
-            var fexp=cad.toGMTString();
-		if(document.referrer != ""){
-			temp=document.referrer+"-URL-"+document.location+"-cookf="+hoy()+"-expireDate="+fexp+"||";
-                        //temp=document.referrer+"-URL-"+document.location;
+	if(leerCookie("Tracking")==false){
+      //console.log("NO ENCONTRO NADA TRAKING");
+      var cad = new Date(new Date().setTime(new Date().getTime()+dias*24*60*60*1000)); // 30 days
+      var fexp = cad.toGMTString();
+
+		if(document.referrer == ""){      
+      temp += 'http://';
+      temp += '-URL='+document.location;
+      temp += '-COOKF='+hoy();
+      temp += '-EXPIRATEDATE='+fexp;
+      temp += '||';
 		}else {
-			temp=document.location+"-cookf="+hoy()+"-expireDate="+fexp+"||";
-                        //temp=document.location;
+      temp += document.referrer;
+      temp += '-URL='+document.location;
+      temp += '-COOKF='+hoy();
+      temp += '-EXPIRATEDATE='+fexp;
+      temp += '||';
 		}
-
-
+    //console.log('temp',temp);
 		cearCookie("Tracking",temp,fexp);
+
 	}else{
+    //console.log("ENCONTRO TRAKING");
 
-            var cooarray=unescape(leerCookie("Tracking")).split('||');
+    var cooarray = unescape(leerCookie("Tracking")).split('||');
+    track = unescape(cooarray[cooarray.length-2]).split('-COOKF=');
 
+    // console.log('trackkkk for split',track);
+    // console.log('track[1]',track[1]);
 
-            track = unescape(cooarray[cooarray.length-2]).split('-cookf=');
+    cookf = track[1].split('-EXPIRATEDATE=');
 
-            cookf = track[1].split('-expireDate=');
+    if(cookf[0]!=hoy()){
+        var urltot="";
+        var p="";
+        for(var i=0;i<cooarray.length-1;i++){         
+         p1=unescape(cooarray[i]).split('-EXPIRATEDATE=');
+         urltot+=p1[0]+"||";         
+        }
+        if(document.referrer != ""){
+          temp=urltot+document.referrer+"-URL="+document.location+"-COOKF="+hoy()+"-EXPIRATEDATE="+cookf[1]+"||";
+        }else{
+          temp=urltot+document.location+"-COOKF="+hoy()+"-EXPIRATEDATE="+cookf[1]+"||";
+        }
 
-            if(cookf[0]!=hoy()){
-                var urltot="";
-                var p="";
-                for(var i=0;i<cooarray.length-1;i++){
-                 //if(cooarray[i]!=''){
-                 p1=unescape(cooarray[i]).split('-expireDate=');
-                 urltot+=p1[0]+"||";
-                   // }
-               }
-                if(document.referrer != ""){
-                  temp=urltot+document.referrer+"-URL-"+document.location+"-cookf="+hoy()+"-expireDate="+cookf[1]+"||";
-                }else{
-                  temp=urltot+document.location+"-cookf="+hoy()+"-expireDate="+cookf[1]+"||";
-                }
-            }else{
-                var urltot="";
-                var p="";
-                for(var i=0;i<cooarray.length-2;i++){
-                 p1=unescape(cooarray[i]).split('-expireDate=');
-                 urltot+=p1[0]+"||";
-               }
-                temp=urltot+track[0]+"-URL-"+document.location+"-cookf="+cookf[0]+"-expireDate="+cookf[1]+"||";
-            }
+    }else{
+        var urltot="";
+        var p="";
+        for(var i=0;i<cooarray.length-2;i++){
+         p1=unescape(cooarray[i]).split('-EXPIRATEDATE=');
+         urltot+=p1[0]+"||";
+       }
+        temp=urltot+track[0]+"-URL="+document.location+"-COOKF="+cookf[0]+"-EXPIRATEDATE="+cookf[1]+"||";
+    }
 		cearCookie("Tracking",temp,cookf[1]);
 	}
 }
 
-var cargar = function () {
-
-		var temp;
-		var valor;
-		if(leerCookie("Tracking")=="" || leerCookie("Tracking")==undefined){
-			if(document.referrer==''){
-				pag_ant='Directamente al LP';
-			}else{
-				pag_ant=document.referrer;
-				cearCookie("Tracking",pag_ant,null);
-			}
-			document.getElementById('tracking_url').value=pag_ant;
-		}else{
-			if(leerCookie("Tracking")!=document.referrer){
-			temp = leerCookie("Tracking");
-				document.getElementById('tracking_url').value=temp;
-			}
-		}
+var cargar = function(){
+    if(leerCookie("Tracking")){
+      document.getElementById('tracking_url').value = leerCookie("Tracking");
+    }
 }
 
 // function setCookieGratis(){
@@ -125,22 +125,14 @@ var cargar = function () {
 
 
 
-function hoy()
-{
+function hoy(){
     var f = new Date();
-//document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
-
-  //  var fechaActual = new Date();
-
     dia = f.getDate();
     mes = f.getMonth() +1;
     anno = f.getFullYear();
 
-
     if (dia <10) dia = "0" + dia;
     if (mes <10) mes = "0" + mes;
-
     fechaHoy = dia + "/" + mes + "/" + anno;
-
     return fechaHoy;
 }
